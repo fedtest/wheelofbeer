cb5d862b16150e26c173f79e27851d2a4addd5ea
Gustaf Hjort af Ornäs <ghiort@gmail.com>
2016-09-27 14:53:45 +0200

    log pin, and only trigger on falling

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const SlackBot = require('slackbots');
 const config = require('./config.json');
 const Gpio = require('onoff').Gpio;
 
-const sensorTest1 = new Gpio(21, 'in', 'both');
-const sensorTest2 = new Gpio(29, 'in', 'both');
-const sensorTest3 = new Gpio(40, 'in', 'both');
+const sensorTest1 = new Gpio(21, 'in', 'falling');
+const sensorTest2 = new Gpio(29, 'in', 'falling');
+const sensorTest3 = new Gpio(40, 'in', 'falling');
 
 const bot = new SlackBot(config);
-function onWatch(){
-    console.log('Watch triggered')
+function onWatch(pin){
+    console.log(`Watch triggered on pin: ${pin}`)
 }
 bot.on('start', () => {
     console.log('Bot is online');
-    sensorTest1.watch(onWatch);
-    sensorTest2.watch(onWatch);
-    sensorTest3.watch(onWatch);
+    sensorTest1.watch(() => onWatch(21));
+    sensorTest2.watch(() => onWatch(29));
+    sensorTest3.watch(() => onWatch(40));
 
     bot.on('message', (data) => {
         if (data.type === 'message') {
