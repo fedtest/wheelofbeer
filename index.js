42d5e598530b1fb2c221e1ca347a43a58555b880
Gustaf Hjort af Ornäs <ghiort@gmail.com>
2016-09-27 14:31:45 +0200

    Added analog input test

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const SlackBot = require('slackbots');
 const config = require('./config.json');
+const Gpio = require('onoff').Gpio;
+
+const sensorTest = new Gpio(4, 'in');
 
 const bot = new SlackBot(config);
 
 bot.on('start', () => {
-
     console.log('Bot is online');
-
+    sensorTest.watch((err) => {
+        if (err) {
+            throw err;
+        }
+        else{
+            bot.postMessage(data.channel, 'Input on 4, Fleming it is.');
+        }
+    } );
     bot.on('message', (data) => {
         if (data.type === 'message') {
             if (data.text.toLowerCase() === 'time for beer') {
