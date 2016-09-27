e7af6dcb0d708b05a4ee87dc61e02584f4af3186
Alex Kott <tetlokax@gmail.com>
2016-09-27 15:12:12 +0200

    added bars and sensors

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const SlackBot = require('slackbots');
 const config = require('./config.json');
+const bars = require('./bars.json');
 const Gpio = require('onoff').Gpio;
 
-const sensorTest1 = new Gpio(21, 'in', 'falling');
-const sensorTest2 = new Gpio(29, 'in', 'falling');
-const sensorTest3 = new Gpio(40, 'in', 'falling');
+const portId = [21, 29, 40];
+const sensors = [];
+
+let barIndex;
+
+portId.forEach(port => sensors.push(new Gpio(port, 'in', 'falling')));
 
 const bot = new SlackBot(config);
-function onWatch(pin){
-    console.log(`Watch triggered on pin: ${pin}`)
-}
+
 bot.on('start', () => {
     console.log('Bot is online');
-    sensorTest1.watch(() => onWatch(21));
-    sensorTest2.watch(() => onWatch(29));
-    sensorTest3.watch(() => onWatch(40));
+    sensors.forEach((sensor, index) => (barIndex = index));
 
     bot.on('message', (data) => {
         if (data.type === 'message') {
             if (data.text.toLowerCase() === 'time for beer') {
                 bot.postMessage(data.channel, 'spinning wheel, wait for it....', {
                      icon_emoji: ':beer:',
+                });
+            } else if (data.text.toLowerCase() === 'trigger bar') {
+                bot.postMessage(data.channel, `We're going to ${bars[barIndex].name}`, {
+                    icon_emoji: ':beers:',
                 });
             }
         }
