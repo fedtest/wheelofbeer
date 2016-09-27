ee9ac89dc4388e22ec70bfcfacbc051349a06341
Tobias Hultman <tobias.hultman@gmail.com>
2016-09-27 15:57:28 +0200

    Added PIN 4 as motor. Starts motor for 10 sec.

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const Gpio = require('onoff').Gpio;
 const portId = [5, 6, 13, 19, 26, 16, 20, 21];
 const sensors = [];
 
+const motor = new Gpio(4, 'out');
+
+const WHEEL_SPINNING = 'SPINNING';
+const WHEEL_IDLE = 'IDLE';
+
+let wheelState = WHEEL_IDLE;
 let barIndex = 0;
 
 portId.forEach(port => sensors.push(new Gpio(port, 'in', 'falling')));
@@ -23,9 +29,21 @@ bot.on('start', () => {
     bot.on('message', (data) => {
         if (data.type === 'message') {
             if (data.text.toLowerCase() === 'time for beer') {
-                bot.postMessage(data.channel, 'spinning wheel, wait for it....', {
-                     icon_emoji: ':beer:',
-                });
+                if (wheelState === WHEEL_SPINNING) {
+                  bot.postMessage(data.channel, 'wheel is still spinning, wait for it...')
+                }
+                else if (wheelState === WHEEL_IDLE) {
+                  bot.postMessage(data.channel, 'spinning wheel, wait for it....', {
+                       icon_emoji: ':beer:',
+                  });
+                  motor.write(1);
+                  wheelState = WHEEL_SPINNING;
+                  setTimeout(() => {
+                    motor.write(0);
+                    wheelState = WHEEL_IDLE;
+                  }, 10000);
+                }
+
             } else if (data.text.toLowerCase() === 'trigger bar') {
                 bot.postMessage(data.channel, `We're going to ${bars[barIndex].name}`, {
                     icon_emoji: ':beers:',
